efc20cede85d61d064e1814fd22aecb4892cf561
olexande <olexande@pisem.net>
2016-08-31 23:41:13 +0300

    BookService.POJO Object L2.2

--- BookService.java
+++ BookService.java
@@ -1,19 +1,11 @@
 public class BookService {
-@Inject
 private NumberGenerator numberGenerator;
-  @Inject
-  private EntityManager em;
-  private Date instanciationDate;
-  @PostConstruct
-  private void initDate() {
-    instanciationDate = new Date();
+  public BookService() {
+    this.numberGenerator = new IsbnGenerator();
   }
-  @Transactional
   public Book createBook(String title, Float price, String description) {
     Book book = new Book(title, price, description);
     book.setIsbn(numberGenerator.generateNumber());
-    book.setInstanciationDate(instanciationDate);
-    em.persist(book);
     return book;
   }
 }
